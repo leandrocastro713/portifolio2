fb2542077d733baa360f242168a57b0a29deedbd
leandrocastro713 <109395934+leandrocastro713@users.noreply.github.com>
2022-12-12 23:45:08 -0300

    hover em knowledges

---
+++ script.js
@@ -0,0 +1,15 @@
+let _front = document.querySelector("#know-front");
+let _social = document.querySelector("#know-social");
+let _dev = document.querySelector("#know-dev");
+let _front_popup = document.querySelector("#know-front-popup");
+let _social_popup = document.querySelector("#know-social-popup");
+let _dev_popup = document.querySelector("#know-dev-popup");
+//"know-front" "know-social" "know-dev"
+_front.addEventListener('mouseenter',()=>{
+    console.log("entrou em frontend");
+
+})
+_front.addEventListener('mouseleave',()=>{
+    console.log("saiu em frontend");
+    
+})
